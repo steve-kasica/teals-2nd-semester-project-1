d74d8eff7930e30ff9599567e7385762f7311e78
Steve Kasica <kasica@cs.ubc.ca>
2019-01-21 07:31:38 -0800

    Complete News Remix

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Space Opera Mad Lib
 
-This file is an example of [Project 1: Mad Libs](https://tealsk12.gitbook.io/intro-cs-2/unit_1/lesson-5/project) for students taking 2nd Semester Intro to Computer Science through [TEALS](https://www.tealsk12.org). It turns the opening crawl from Star Wars: A New Hope into a Mad Lib with Python 3.
+This file is a jazzed-up example of [Project 1: Mad Libs](https://tealsk12.gitbook.io/intro-cs-2/unit_1/lesson-5/project) for students taking 2nd Semester Intro to Computer Science through [TEALS](https://www.tealsk12.org). It turns a random story from the front page of today's *New York Times* into a Mad Lib using part-of-speech tagging in Python 3.
+
+## Requirements
+
+In order to run this program, you will need your own API key. See[the developer website for instructions to [obtain your own key](https://developer.nytimes.com/get-started).
 
 ## Disclaimer
 Although there are other, arguabily better, ways to structure Python code, write docstrings, concatenate strings, etc, the conventions in this file reflect the current knowledge of students taking the course.
--- main.py
+++ main.py
@@ -1,33 +1,48 @@
+import requests
+import nltk
+from nltk import word_tokenize
+from random import randint
+
+key = 'HO5sagAkDfE09Wv4DFOudFAou8gcTRpL'
 
 # Step 1: Display story title
-
-title = 'Space Opera: a Mad Lib.'
+title = 'Remix the News: a Mad Lib'
 print(title)
 
 welcome = 'Welcome! You are about to play a fantastic word game. I will ask you for nouns, verbs, adjectives, and proper nouns. Using those words I will create an unexpected story for you!'
-print(welcome)
+print(welcome, '\n')
 
-# Step 2: Prompt user for words
+# Step 2: Get the summary of a random story on the front page of The New York Times
+url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=' + key
+res = requests.get(url)
+articles = res.json()['results']
+index = randint(0, len(articles))
+abstract = articles[index]['abstract']
 
-adj1 = input('Give me an adjective: ')
-noun1 = input('Give me a plural noun: ')
-adj2 = input('Give me another adjective: ')
-noun2 = input('Give me a proper noun: ')
-noun3 = input('Give me a different plural noun: ')
-verb1 = input('Give me a verb: ')
-noun4 = input('Give me another proper noun: ').upper()
-noun5 = input('Give me a different noun: ')
-verb2 = input('Give me a different verb: ')
-noun6 = input('Who is the coolest girl in school? ')
+# Step 3: Tag the part of speech for each word in the article's abstract
+text = word_tokenize(abstract)
+tags = nltk.pos_tag(text)
 
-# Step 3: Assemble story
+# Step 4: Prompt the user to replace nouns, verbs, and adjectives from the article's abstract
+story = ''
+for tag in tags:
+    word = ''
+    pos = tag[1]  # the second item in the tuple is the Part Of Speech
+    if (pos == 'NN'):  # NN means noun
+        word = input('Give me a noun: ')
+    elif (pos == 'NNS'):  # NNS means plural noun
+        word = input('Give me a plural noun: ')
+    elif (pos == 'VB'):  # VB means verb
+        word = input('Give me a verb: ')
+    elif (pos == 'NNP'):  # NNP means proper noun
+        word = input('Give me a proper noun: ')
+    elif (pos == 'NNPS'):  # NNPS means plural proper noun
+        word = input('Give me a plural proper noun: ')
+    elif (pos == 'JJ'):  # JJ means adjective
+        word = input('Give me an adjecive: ')
+    else:
+        word = tag[0]
+    story = story + word + ' '
 
-story1 = 'It is a period of ' + adj1 + ' war. Rebel '  + noun1 + ' striking from a hidden base, have won their ' + adj2 + ' victory against the evil ' + noun2 + '. ' 
-
-story2 = 'During the battle, Rebel ' + noun1 + ' managed to ' + verb1 + ' secret ' + noun3 + ' to the ' + noun2 + '\'s ultimate weapon, the ' + noun4 + ', an armored space station with enough ' + noun5 + ' to ' + verb2 + ' an entire planet. '
-
-story3 = 'Pursued by ' + noun2 + '\'s sinister agents, Princess ' + noun6 + ' races home aboard her starship, custodian of the stolen ' + noun3 + ' that can save her people and restore freedom to the galaxy.....'
-
-# Step 4: Display story
-
-print(story1 + story2 + story3)
+# Step 5: Display MadLib story back to user
+print('\n' + story)
